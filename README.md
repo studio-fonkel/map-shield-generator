6469c82186b760021eb3276b914de2b0c075245c
Laurens Bruijn <laurens@studiofonkel.nl>
2017-05-16 12:09:39 +0200

    v1

---
+++ README.md
@@ -0,0 +1,27 @@
+# Map Shield Generator
+
+## Usage:
+`npm install`
+`node index.js letterWidth letterHeight paddingX paddingY r,g,b,a amount`
+
+e.g.:
+`node index.js 10 30 5 5 100,100,100,255 20`
+
+### letterWidth
+Width of a single letter
+
+### letterHeight
+Height of a single letter
+
+### paddingX:
+Padding on both sides
+
+### paddingY
+Padding on top and bottom
+
+### r,g,b,a
+Color in rgba (values 0-255).
+E.g. `255,0,0,255`
+
+### amount:
+Amount of images to generate (same as max. letters to support).
--- index.js
+++ index.js
@@ -2,31 +2,105 @@
 
 const Jimp = require('jimp');
 
+// Defaults:
+var options = {
+  letterWidth: 10,
+  letterHeight: 10,
+  paddingX: 5,
+  paddingY: 5,
+  borderColor: 0xFF000000,
+  amount: 1,
+  folder: 'images/',
+  filenamePrefix: 'image-'
+}
+
+// Set options:
 process.argv.forEach(function (val, index, array) {
-  console.log(index + ': ' + val);
+  switch(index) {
+    case 2:
+      options.letterWidth = parseInt(val);
+      break;
+    case 3:
+      options.letterHeight = parseInt(val);
+      break;
+    case 4:
+      options.paddingX = parseInt(val);
+      break;
+    case 5:
+      options.paddingY = parseInt(val);
+      break;
+    case 6:
+      let values = val.split(",");
+      let r = parseInt(values[0]);
+      let g = parseInt(values[1]);
+      let b = parseInt(values[2]);
+      let a = parseInt(values[3]);
+      options.borderColor = Jimp.rgbaToInt(r, g, b, a);
+      break;
+    case 7:
+      options.amount = parseInt(val);
+      break;
+    case 8:
+      options.filenamePrefix = val;
+      break;
+  }
+
 });
 
-// node index.js 10 20 1 222222
+// node index.js 10 20 5 3 1 0,0,0,255 5
+// node index.js letterWidth letterHeight paddingX paddingY r,g,b,a amount
 
-let imageData = [
-  [ 0xFF0000FF, 0xFF0000FF, 0xFF0000FF ],
-  [ 0xFF0000FF, false, 0xFF0000FF ],
-  [ 0xFF0000FF, 0xFF0000FF, 0x0000FFFF ]
-];
+for (var i = 1; i <= options.amount; i++) {
 
+  // Set current image dimensions:
+  let imageWidth = options.letterWidth * i + (options.paddingX * 2);
+  let imageHeight = options.letterHeight + (options.paddingY * 2);
 
-let image = new Jimp(30, 30, function (err, image) {
-  if (err) throw err;
+  // Create image:
+  let image = new Jimp(parseInt(imageWidth), parseInt(imageHeight), function (err, image) {
+    if (err) throw err;
 
-  imageData.forEach((row, y) => {
-    row.forEach((color, x) => {
-      if (color) {
-        image.setPixelColor(color, x, y);
+    // Each row of pixels:
+    for (var y = 0; y < imageHeight; y++) {
+      let border = 0;
+
+      switch(y) {
+        case 0:
+          border = 1;
+          break;
+        case imageHeight-1:
+          border = 1;
+          break;
       }
+      if (border) {
+        // Each column that needs a border:
+        for (var x = 0; x < imageWidth; x++) {
+          image.setPixelColor(options.borderColor, x, y);
+        }
+      }
+    }
+    // Each column:
+    for (var x = 0; x < imageWidth; x++) {
+      let border = 0;
+
+      switch(x) {
+        case 0:
+          border = 1;
+          break;
+        case imageWidth-1:
+          border = 1;
+          break;
+      }
+      if (border) {
+        for (var y = 0; y < imageHeight; y++) {
+          image.setPixelColor(options.borderColor, x, y);
+        }
+      }
+    }
+
+    image.write(options.folder + options.filenamePrefix + i +'.png', (err) => {
+      if (err) throw err;
     });
   });
 
-  image.write('test.png', (err) => {
-    if (err) throw err;
-  });
-});
+}
